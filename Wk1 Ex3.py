04226738f294a04c8811aa81928964a387aa8643
23lotte23 <77005658+23lotte23@users.noreply.github.com>
2021-02-08 22:11:56 +0000

    Add files via upload

---
+++ Wk1 Ex3.py
@@ -0,0 +1,5 @@
+input_1= input ("What is your favourite starter?  ")
+input_2= input ("What is your favourite main course? ")
+input_3= input ("What is your favourite drink? ")
+
+print ("Your favorite meal is " + input_1 + ", " + input_2 + " with a glass of " + input_3)
