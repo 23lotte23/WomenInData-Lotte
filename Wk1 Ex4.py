e7694a94b18c40c026890c83b0c7f45cde2532af
23lotte23 <77005658+23lotte23@users.noreply.github.com>
2021-02-08 22:15:10 +0000

    Add files via upload

---
+++ Wk1 Ex4.py
@@ -0,0 +1,6 @@
+year = 1
+number = 2000
+while number > 1000:
+  print ("Year", year, "=£", number)
+  year = year +1
+  number = number * 0.9
---
+++ Wk1 Ex5.py
@@ -0,0 +1,19 @@
+input_a = input ("Please think of a number? ")
+input_b = input ("Please think of another number? ")
+f = input ("Please think of a mathamatical function. Please type in the relevant letter + (a), - (b) , * (c), / (d), squared (e) or to the power of (f): ")
+if f == "a":
+  print (sum (float((input_a) + (input_b)))
+if f == "b":
+  (sum (float((input_a) - (input_b)))
+if f == "c":
+  (sum(float(input_a * input_b)))
+if f == "d":
+  (sum(float(input_a)/ (input_b)))
+if f == "e":
+  print (float({input_a}, (sum(input_a * input_a)))
+  print (float({input_b}, (sum(input_b * input_b))))
+else:
+  print (sum(float(input_a ** input_b)))
+
+
+  
