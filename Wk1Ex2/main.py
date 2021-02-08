58f41aa82d857ffed3ee93640cf964fa771fe171
23lotte23 <77005658+23lotte23@users.noreply.github.com>
2021-02-08 22:10:16 +0000

    Add files via upload

---
+++ Wk1Ex2/main.py
@@ -0,0 +1,8 @@
+user_imput = input("What is your name? ")
+number = print(user_imput ," ,would you like me to tell you a joke? Please pick a number between 1 and 100")
+if number = < 34:
+    print("Whats brown and sticky? A stick")
+elif number = < 66:
+    print("Money talks, all mine ever says in goodbye.")
+else:
+    print("Moses had the first tablet that could connect ot the cloud.")
