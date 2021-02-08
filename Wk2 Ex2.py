f450815545da94b704e1afc4d2bf1ab04d001469
23lotte23 <77005658+23lotte23@users.noreply.github.com>
2021-02-08 22:15:48 +0000

    Add files via upload

---
+++ Wk2 Ex2.py
@@ -0,0 +1,9 @@
+def function_1():
+  year = 1
+  number = 2000
+  while number > 1000:
+    year = year +1
+    number = number * 0.9
+
+  function = ("Year", year ,"=£", number)
+output = function_1
---
+++ Wk2 ex1.py
@@ -0,0 +1,6 @@
+ans_1 = bool(input("Would you like our special? "))
+ans_2 = input("What would you like? ")
+if (ans_1 == yes):
+    print("thank you for your custom, that will be £3.50")
+else:
+    print(ans_2)
